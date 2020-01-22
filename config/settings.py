0bb23afe61e3b87370b61587546eaab116119fda
오승찬 <khx1712@naver.com>
2020-01-22 23:55:56 +0900

    linkage AWS_S3

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'disqus',
     'django.contrib.sites',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,12 @@ LOGIN_REDIRECT_URL = '/'
 
 DISQUS_WEBSITE_SHORTNAME = 'dstagram-khx1712'
 SITE_ID=1
+
+AWS_ACCESS_KEY_ID = 'AKIA4QOSHYEFI6EEQNOO'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_REGION = 'ap-northeast-2'
+AWS_STORAGE_BUCKET_NAME = 'dstagram-khx1712'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS={'CacheControl':'max-age=86400',}
+DEFAULT_FILE_STORAGE='config.asset_storage.MediaStorage'
+
